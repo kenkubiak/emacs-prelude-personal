03c01cfa2f88cb9d32abd0648297922c0b2d6840
Ken Kubiak <ken@mnmlst.me>
2022-10-17 10:20:06 -0700

    add frontside js and some themes

--- custom.el
+++ custom.el
@@ -5,19 +5,48 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(tango-dark))
  '(custom-safe-themes
-   '("2dc03dfb67fbcb7d9c487522c29b7582da20766c9998aaad5e5b63b5c27eec3f" default))
+   '("1f6039038366e50129643d6a0dc67d1c34c70cdbe998e8c30dc4c6889ea7e3db" "2f08b4f5ff619bdfa46037553ea41f72f09013a2e6b7287799db6cec6a7dddb2" "78e6be576f4a526d212d5f9a8798e5706990216e9be10174e3f3b015b8662e27" "2dc03dfb67fbcb7d9c487522c29b7582da20766c9998aaad5e5b63b5c27eec3f" default))
  '(package-selected-packages
-   '(rjsx-mode prettier-js prettier tide projectile-speedbar speedbar yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode ts-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window)))
+   '(seti-theme monokai-theme melancholy-theme use-package frontside-javascript rjsx-mode prettier-js prettier tide projectile-speedbar speedbar yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode ts-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#24221d" :foreground "#ddd" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "apple" :family "Input Mono Narrow"))))
+ '(bold ((t (:weight bold))))
+ '(button ((t (:inherit link))))
+ '(fixed-pitch ((t (:family "Input Mono Narrow"))))
+ '(mode-line ((t (:background "#e6e5e4" :foreground "black"))))
+ '(mode-line-buffer-id ((t (:foreground "firebrick4"))))
+ '(mumamo-background-chunk-submode1 ((t (:background "#101820"))))
+ '(tool-bar ((t (:background "grey75" :foreground "black" :box 2))))
+ '(variable-pitch ((t (:family "Input Sans Narrow"))))
+ '(whitespace-space ((t (:foreground "#444"))))
+ '(whitespace-tab ((t (:foreground "#444")))))
+
+;; Package setup
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  (add-to-list 'load-path "~/.emacs.d/elpa/use-package-20221012.1743")
+  (require 'use-package))
+
+;; Themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/personal/themes/")
+
+(use-package melancholy-theme
+  :ensure t)
+
+(use-package monokai-theme
+  :ensure t)
+
+(use-package seti-theme
+  :ensure t)
+
+(load-theme 'seti t)
 
 ;; use speedbar in the same frame
-(sr-speedbar-open)
 (global-set-key (kbd "s-|") 'sr-speedbar-toggle)
 
 ;; Use font Input Mono
@@ -66,3 +95,14 @@
 
 (add-hook 'web-mode-hook 'company-mode)
 (add-hook 'web-mode-hook 'prettier-js-mode)
+
+;; SHELL and PATH for subshells
+(setq explicit-shell-file-name "/bin/bash" )
+(setq exec-path
+      (append
+       '("/Users/kk/.nvm/versions/node/v16.14.2/bin")
+       exec-path))
+
+;; Frontside Javascript
+(use-package frontside-javascript
+             :init (frontside-javascript))
